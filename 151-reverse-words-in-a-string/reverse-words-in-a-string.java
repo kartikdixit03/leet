2289e69022da5c25d91206252154fcc430c63667
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2024-04-11 00:18:34 +0530

    Time: 13 ms (19.13%) | Memory: 44.9 MB (31.42%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,14 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> stack = new Stack<>();
-        String[] words = s.split("\\s+");
-        for (String word : words) {
-            stack.push(word);
+        String[] arr = s.split("\\s+");
+        s = "";
+        for(int i = arr.length-1; i>=0; i--) {
+            s += (arr[i]+ " ");   
         }
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop() + " ";
-        }
-        return reversed.trim(); 
+        return s.trim();
     }
 }
