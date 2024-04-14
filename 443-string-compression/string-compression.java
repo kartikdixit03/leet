e519a973965168b4d24a70df9714c266f2e4849c
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2024-04-15 00:37:55 +0530

    Time: 1 ms (99.58%) | Memory: 43.4 MB (90.34%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,23 +1,26 @@
 class Solution {
     public int compress(char[] chars) {
        StringBuilder str = new StringBuilder();
-        int count = 1;
+    str.append(chars[0]);
+    int count = 1;
 
-        for (int i = 1; i <= chars.length; i++) {
-            if (i < chars.length && chars[i] == chars[i - 1]) {
-                count++;
-            } else {
-                str.append(chars[i - 1]);
-                if (count > 1) {
-                    str.append(count);
-                }
+    for (int i = 1; i < chars.length; i++) {
+        if (chars[i] == chars[i - 1]) {
+            count++;
+        } else {
+            if (count > 1) {
+                str.append(count);
                 count = 1;
             }
-            if (i == chars.length) {
-                break;
-            }
+            str.append(chars[i]);
         }
-        str.getChars(0, str.length(), chars, 0);
-        return str.length();
+    }
+    if (count > 1) {
+        str.append(count);
+    }
+      for(int i = 0; i < str.length(); i++) {
+            chars[i] = str.charAt(i);;
+    }
+    return str.length();
     }
 }
