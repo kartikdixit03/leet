bc2b0686e2b6c81d0966339c24a3a85874c01940
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-02-04 17:34:36 +0530

    Time: 7 ms (8.32%) | Memory: 69 MB (96.47%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -3,7 +3,7 @@ class Solution {
         int left =0;
         double sum = 0;
         double maxAverage = Integer.MIN_VALUE;
-        double aMax  =0;
+    
         for(int right =0; right<nums.length; right++){
                 sum += nums[right];
                 if(right - left +1 >k){
@@ -11,7 +11,7 @@ class Solution {
                     left++;
                 }
                 if(right - left +1 == k){
-                aMax = sum/k;
+               double aMax  = sum/k;
 
                 maxAverage = Math.max(maxAverage,aMax);
                 }
