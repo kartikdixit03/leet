9e55ac986afc21d426343ba573842890e113e3ac
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-02-05 00:58:17 +0530

    Time: 1 ms (65.51%) | Memory: 43.3 MB (45.49%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        char [] first = strs[0].toCharArray();
+        char [] last = strs[strs.length-1].toCharArray();
+        StringBuilder prefix = new StringBuilder();
+        for(int i =0; i<first.length;i++){
+        if(first[i] != last[i]){
+            break;
+        }else{
+        prefix.append(first[i]);
+            }
+        }
+        // for (int i = 0; i < first.length; i++) {
+        //     if (first[i] != last[i]) {
+        //         break;
+        //     } else {
+        //         prefix.append(first[i]);
+        //     }
+        // }
+        return prefix.toString();
+    }
+}
