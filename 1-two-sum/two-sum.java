50935c0008a98ec409fdb74430aac5368023e581
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-03-05 18:21:49 +0530

    Time: 2 ms (99.21%) | Memory: 47 MB (83.91%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,22 +1,16 @@
 class Solution {
-   public int[] twoSum(int[] nums, int target) {
-   
-       HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // Loop through the array to find a pair that sums up to the target
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            
-            // If the complement exists, return the indices of the pair
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i}; // Valid solution found
-            }
-            
-            // Store the current number and its index
-            map.put(nums[i], i);
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer,Integer> map = new HashMap<>();
+       int diff =0;
+       for(int i=0 ;i< nums.length;i++){
+        diff = target-nums[i];
+        if(map.containsKey(diff)){
+            return new int []{map.get(diff),i};
         }
-
-        // Return a special value when no solution is found
-        return null; 
-    }
+        else{
+            map.put(nums[i],i);
+        }
+        }
+        return new int[] {-1,-1};
+       }    
 }
