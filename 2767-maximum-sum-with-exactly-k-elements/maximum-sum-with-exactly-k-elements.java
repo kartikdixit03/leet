703b2236bdfd3c58fb0f77ec6caffcc45e00ac2a
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-06-03 00:05:59 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (58.59%) - LeetSync

---
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int max =0;
+        for(int i =0; i<nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        int sum =  max*k;
+        sum += (k*(k-1))/2;
+        return sum;
+
+            
+
+    }
+}
