9da84ff70e083ed7af12c9d71b3e8ac7329074d7
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-06-15 01:35:18 +0530

    Time: 3 ms (26.81%) | Memory: 65 MB (75.43%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,41 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int product =1;
         int n = nums.length;
-        int zerCount = 0;
-        for(int i =0; i<n; i++){
-            if(nums[i] == 0){
-                zerCount++;
-                if(zerCount>1){
-                    product =0;
-                    break;
-                } 
-            }
-            else{
-                product= nums[i] * product;
-            }
+        int [] prefix = new int[n];
+        int [] suffix = new int[n];
+        int [] result = new int[n];
+
+         prefix[0] =1;
+        for(int i =1; i<=n-1; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+         suffix[n-1] =1;
+        for(int i =n-2; i>=0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
         }
 
         for(int i =0; i<n; i++){
-            if(zerCount == n){
-                return nums;
-            }
+            result[i] = prefix[i] * suffix[i];
 
-            if(product == 0){
-                nums[i]=0;
-            }
-            else if(zerCount==1){
-                if(nums[i]!=0){
-                    nums[i] =0;
-                }else{
-                    nums[i] = product;
-                }
-            }
-            
-            else{
-                nums[i] = product/nums[i];
-            }
         }
-        return nums;
+
+        return result;
+
     }
 }
