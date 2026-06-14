93e7ce68a2d94b10103831aa1307eeca3204b6e8
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-06-14 23:54:03 +0530

    Time: 22 ms (27.70%) | Memory: 94.9 MB (23.64%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,9 +3,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer,Integer> count = new HashMap<>();
         for(int n: nums){
-            if(count.containsKey(n)){
-                return true;
-            }
+            if(count.containsKey(n)) return true;
             count.put(n,count.getOrDefault(n,0)+1);
         }
         return false;
