1d3a806d46781f7ef7d2b62f810e7e952a0b5510
kartikdixit03 <57520375+kartikdixit03@users.noreply.github.com>
2026-06-23 02:09:40 +0530

    Time: 878 ms (8.88%) | Memory: 60 MB (17.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,19 +1,12 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
-
         Set<List<Integer>> set = new HashSet<>();
-
         for (int i = 0; i < nums.length - 2; i++) {
-
             int l = i + 1;
             int r = nums.length - 1;
-
             while (l < r) {
-
                 int sum = nums[i] + nums[l] + nums[r];
-
                 if (sum == 0) {
                     set.add(Arrays.asList(nums[i], nums[l], nums[r]));
                     l++;
@@ -22,10 +15,7 @@ class Solution {
                     l++;
                 } else {
                     r--;
-                }
-            }
-        }
-
+                }}}
         return new ArrayList<>(set);
     }
 }
